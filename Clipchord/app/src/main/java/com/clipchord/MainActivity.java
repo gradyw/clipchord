ce29e0b6ebaae0b87673fd0b9d9c97d15f5057c2
danieltsai-RSheepDV <leinazheshensi@gmail.com>
2020-03-29 00:19:13 -0700

    Code Cleanup

--- Clipchord/app/src/main/java/com/clipchord/MainActivity.java
+++ Clipchord/app/src/main/java/com/clipchord/MainActivity.java
@@ -12,6 +12,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -37,16 +38,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 123;
 
     Button signInButton;
-    Button fileButton;
     static FirebaseUser user;
-
-    public static FirebaseUser getUser() {
-        return user;
-    }
-
-    public static FirebaseStorage getStorage() {
-        return storage;
-    }
 
     public static StorageReference getStorageRef() {
         return storageRef;
@@ -55,130 +47,23 @@ public class MainActivity extends AppCompatActivity {
     private static FirebaseStorage storage = FirebaseStorage.getInstance();
     private static StorageReference storageRef = storage.getReference();
 
-    private static final int STORAGE_REQUEST_CODE = 100;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (!checkPermissionForReadExtertalStorage()) {
-            try {
-                requestPermissionForReadExtertalStorage();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        // Choose authentication providers
-
-        // Create and launch sign-in intent
-
         signInButton = findViewById(R.id.SIGNIN);
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
+        signInButton.setOnClickListener(v -> {
+            List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
 
-                if (ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.READ_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    // Permission is not granted
-                    ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                            STORAGE_REQUEST_CODE);
-                }
-
-                if (ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    // Permission is not granted
-                    ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                            STORAGE_REQUEST_CODE);
-                }
-
-
-                MainActivity.this.startActivityForResult(
-                        AuthUI.getInstance()
-                                .createSignInIntentBuilder()
-                                .setAvailableProviders(providers)
-                                .build(),
-                        RC_SIGN_IN);
-            }
+            MainActivity.this.startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN);
         });
-
-//        fileButton = findViewById(R.id.button);
-//        fileButton.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//
-//                Uri file = Uri.fromFile(new File("sdcard/Download/test.txt"));
-//                StorageReference fileRef = storageRef.child(file.getLastPathSegment());
-//                UploadTask uploadTask = fileRef.putFile(file);
-//
-//// Register observers to listen for when the download is done or if it fails
-//                uploadTask.addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception exception) {
-//                        System.out.println("Unsuccessful Upload");
-//                    }
-//                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-//                    @Override
-//                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-//                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
-//                        // ...
-//                    }
-//                });
-//            }
-
-//            @Override
-//            public void onClick(View v) {
-//                StorageReference pathReference = storageRef.child("TestFile.txt");
-//                StorageReference gsReference = storage.getReferenceFromUrl("gs://clipchord.appspot.com/TestFile.txt");
-//                StorageReference httpsReference = storage.getReferenceFromUrl("https://firebasestorage.googleapis.com/v0/b/clipchord.appspot.com/o/TestFile.txt");
-//
-//                try {
-//                    File localFile  = File.createTempFile("test","txt");
-//                    pathReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-//                        @Override
-//                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-//                            System.out.println("FILE SUCCESS");
-//                        }
-//                    }).addOnFailureListener(new OnFailureListener() {
-//                        @Override
-//                        public void onFailure(@NonNull Exception e) {
-//                            System.out.println("FILE FAILURE");
-//                        }
-//                    });
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-
-
-    }
-
-    public boolean checkPermissionForReadExtertalStorage() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            int result = this.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
-            return result == PackageManager.PERMISSION_GRANTED;
-        }
-        return false;
-    }
-
-    public void requestPermissionForReadExtertalStorage() throws Exception {
-        try {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                    ContextCompat.checkSelfPermission(this,
-                            Manifest.permission.READ_EXTERNAL_STORAGE));
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw e;
-        }
     }
 
     @Override
@@ -189,18 +74,12 @@ public class MainActivity extends AppCompatActivity {
             IdpResponse response = IdpResponse.fromResultIntent(data);
 
             if (resultCode == RESULT_OK) {
-                // Successfully signed in
                 user = FirebaseAuth.getInstance().getCurrentUser();
-                System.out.println("Sign In Succeeded");
+                Log.d("Auth", "Sign In Succeeded");
                 System.out.println(user.getEmail());
                 startActivity(new Intent(this, UploadTestActivity.class));
-                // ...
             } else {
-                System.out.println("Sign In Failed");
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
+                Log.d("Auth", "Sign In Failed");
             }
         }
     }
--- Clipchord/app/src/main/java/com/clipchord/UploadTestActivity.java
+++ Clipchord/app/src/main/java/com/clipchord/UploadTestActivity.java
@@ -1,7 +1,5 @@
 package com.clipchord;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,8 +9,6 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -28,56 +24,21 @@ public class UploadTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uploadtest);
 
-
-
-
-//        if (ContextCompat.checkSelfPermission(this,
-//                Manifest.permission.READ_EXTERNAL_STORAGE)
-//                != PackageManager.PERMISSION_GRANTED) {
-//
-//
-//            // OPCIONAL(explicaciones de poque pedimos los permisos)
-//            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-//                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
-//
-//            } else {
-//                //pedir permisos
-//                ActivityCompat.requestPermissions(this,
-//                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-//                        permissionCheck);
-//            }
-//        }
-
         Button button = findViewById(R.id.upload);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener((v) -> {
+            File uploadFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Download/test.txt");
+            System.out.println("File Exists" + uploadFile.exists());
 
-            @Override
-            public void onClick(View v) {
+            Uri file = Uri.fromFile(uploadFile);
+            StorageReference fileRef = MainActivity.getStorageRef().child(file.getLastPathSegment());
+            UploadTask uploadTask = fileRef.putFile(file);
 
-
-
-                File uploadFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Download/test.txt");
-                System.out.println("File Exists" + uploadFile.exists());
-
-                Uri file = Uri.fromFile(uploadFile);
-                StorageReference fileRef = MainActivity.getStorageRef().child(file.getLastPathSegment());
-                UploadTask uploadTask = fileRef.putFile(file);
-
-// Register observers to listen for when the download is done or if it fails
-                uploadTask.addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        System.out.println("Unsuccessful Upload");
-                    }
-                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
-                        // ...
-                    }
-                });
-            }
-         });
+            uploadTask.addOnFailureListener(exception -> System.out.println("Unsuccessful Upload")).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                }
+            });
+        });
     }
 }

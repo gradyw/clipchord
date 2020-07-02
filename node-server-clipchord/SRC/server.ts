14118afd00929cb387e211a74c363e1c48d15e28
danieltsai-RSheepDV <leinazheshensi@gmail.com>
2020-07-01 17:38:46 -0700

    Port Forward Publicly

--- node-server-clipchord/SRC/server.ts
+++ node-server-clipchord/SRC/server.ts
@@ -24,7 +24,7 @@ const server = http.createServer((req:any, res:any) => {
     res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 

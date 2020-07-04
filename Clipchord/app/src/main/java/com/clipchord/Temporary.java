f82b1d3adf812cd40fe7c155f67eb30c2a6604f1
danieltsai-RSheepDV <leinazheshensi@gmail.com>
2020-07-03 17:28:08 -0700

    Emptied App of Firebase

---
+++ Clipchord/app/src/main/java/com/clipchord/Temporary.java
@@ -0,0 +1,14 @@
+package com.clipchord;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Temporary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temporary);
+    }
+}
